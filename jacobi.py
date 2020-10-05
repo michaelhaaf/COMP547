f95f05ff0fe14b5709d95453d1edaba66450d513
michaelhaaf <michael.haaf@gmail.com>
2020-10-05 15:33:45 -0400

    create jacobi number calc script

---
+++ jacobi.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys
+
+# Input: non-negative integers a, b 
+# Output: (a / b), the Jacobi symbol of a w.r.t. b
+def jacobi(a, b):
+
+	print("\nJacobi({}, {})".format(a, b))
+	if a <= 1:
+		print(" = {}".format(a))
+		return a
+
+	if a % 2 != 0:
+
+		if a % 4 == 3 and b % 4 == 3:
+			print(" = -Jacobi({} mod {}, {})".format(b, a, a))
+			return -jacobi(b % a, a) 
+		else: 
+			print(" = Jacobi({} mod {}, {})".format(b, a, a))
+			return jacobi(b % a, a)
+
+	else:
+		
+		if b % 8 == 1 or b % 8 == 7:
+			print(" = Jacobi({} / 2, {})".format(a, b))
+			return jacobi(a/2, b)
+		else:
+			print(" = -Jacobi({} / 2, {})".format(a,b))
+			return -jacobi(a/2, b)
+
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+print("\nJacobi({},{}) = {}".format(a, b, jacobi(a,b)))
